4149a2d3426c98a2f31ddd5e0184a41f9f0808ab
Ahmet Kasap <aahmetkasap@gmail.com>
2024-02-01 15:24:18 +0300

    testtr

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const user = JSON.parse(fs.readFileSync('user.json', 'utf8'));
 
 app.get('/', (req,res) => {
-    res.send('start')
+    res.send('ahmet')
 })
 
 app.get('/user', (req,res) => {
